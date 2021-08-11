e0572ae6676957070171fc775274319040e8ffc3
github@94 <deepak.bothra456@gmail.com>
2021-08-11 15:20:14 +0530

    added git demo

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/getTest")
+	public ResponseEntity<String> getDate(){
+		return new ResponseEntity<>("Deepak",HttpStatus.OK);
+		
+	}
+}
